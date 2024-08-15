3fa86e0ee2cce5f9b60f217012a9d34d964d6d2e
xisen <1376260231@qq.com>
2024-08-15 19:34:56 +0800

    添加了一个自由相机，使用Input Touch类来实现用户拖动屏幕时可以滑动屏幕，并且限制显示的范围

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,46 +6,121 @@ using UnityEngine.UI;
 
 public class InputManager : MonoSingleton<InputManager>
 {
-    public Vector2 touchStartPosition;
+    public Vector2 startPosition;
+    public Vector2 lastFramePosition;
     public Vector2 moveVector;
+    public float moveSpeed;
     public Text text;
+    public Vector2 touchPosition;
+    public Touch currentMoveTouch;
+    public float currentFingerId;
+    public Vector2 currentDealtaPosition;
+    public Ray myray;
+    public RaycastHit2D hit;
+
+    private void Start()
+    {
+
+
+        currentMoveTouch.fingerId = -1;
+
+    }
+
 
     private void Update()
     {
-        //if(Input.touchCount > 0)
-        //{
-        //    Touch touch = Input.touches[0];
-        //    if(touch.phase == TouchPhase.Began)
-        //    {
-        //        touchStartPosition = touch.position;
-        //    }
-        //    if (touch.phase == TouchPhase.Moved)
-        //    {
-        //        moveVector = touch.position - touchStartPosition;
-        //        CameraManager.Instance.MoveCameraPosition(moveVector);
-        //    }
-            //foreach (var item in Input.touches)
-            //{
-            //    print( $" 手指ID:{item.fingerId}  手指位置：{item.position} 手指按压：{item.pressure} 手指阶段：{item.phase} 手指类别:{item.type} 点击次数：{item.tapCount}");
-            //}
-        //}
+        currentFingerId = currentMoveTouch.fingerId;
+        currentDealtaPosition = currentMoveTouch.deltaPosition;
+        print("几个手指" + Input.touchCount);
+        if (Input.touchCount > 0)
+        {
+            
+            foreach (var touch in Input.touches)
+            {
+                print($"touch的 fingerID: {touch.fingerId}   Position: {touch.position}   Phase: {touch.phase}  Dealta:{touch.deltaPosition} ");
+                print($" fingerID: {touch.fingerId}   Position: {touch.position}   Phase: {touch.phase}  Dealta:{touch.deltaPosition} ");
+                if (touch.phase == TouchPhase.Began)
+                {
+                    touchPosition = touch.position;
+                    myray = Camera.main.ScreenPointToRay(touchPosition);
+                    hit = Physics2D.Raycast(myray.origin, myray.direction, 20f);
+                    if (hit.collider != null)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        currentMoveTouch = touch;
+                    }
+                }
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    if (currentMoveTouch.fingerId == touch.fingerId)
+                    {
+                        currentMoveTouch = touch;
+                    }
+                }
+                if (touch.phase == TouchPhase.Stationary)
+                {
+                    if (currentMoveTouch.fingerId == touch.fingerId)
+                    {
+                        currentMoveTouch = touch;
+                    }
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    if (currentMoveTouch.fingerId == touch.fingerId)
+                    {
+                        currentMoveTouch.fingerId = -1;
+                    }
+                }
+            }
+        }
+
+        if (currentMoveTouch.fingerId != -1)
+        {
+            moveVector = currentMoveTouch.deltaPosition;
+            transform.Translate(-moveVector.normalized * moveSpeed);
+            if(transform.position.x < -62.67f || transform.position.x > -1.12f || transform.position.y < -27.7f || transform.position.y > -7.5f)
+            {
+                transform.position = new Vector2(Mathf.Clamp(transform.position.x, -62.67f, -1.12f), Mathf.Clamp(transform.position.y, -27.7f, -7.5f));
+            }
+           
+
+        }
 
     }
 
-    private void OnMouseDown()
+
+    //private void OnMouseDown()
+    //{
+    //    text.text = "OnMouseDown + Camera";
+    //    lastFramePosition = Input.mousePosition;
+
+    //}
+
+    //private void OnMouseUp()
+    //{
+
+
+    //}
+
+    //private void OnMouseDrag()
+    //{
+    //    text.text = "OnMouseDrag + Camera";
+    //    moveVector = (lastFramePosition - (Vector2)Input.mousePosition).normalized * moveSpeed;
+    //    transform.Translate(moveVector);
+
+    //}
+
+    private void OnDrawGizmos()
     {
-        text.text = "OnMouseDown + Camera";
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(myray.origin,myray.direction * 20f);
 
     }
 
-    private void OnMouseUp()
-    {
-        text.text = "OnMouseUp + Camera";
-    }
-
-    private void OnMouseDrag()
-    {
-        text.text = "OnMouseDrag + Camera";
-    }
 
 }
